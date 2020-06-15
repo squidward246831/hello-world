a43caec0b8e6be562c4926105b2975088cb85806
Your Name <you@example.com>
2020-06-15 14:44:47 -0500

    ha

--- forms/application.py
+++ forms/application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("/Users/advayramakrishnan/hello-world/forms/index.html")
+    return render_template("/Users/advayramakrishnan/hello-world/forms/templates/index.html")
 
 @app.route("/hello", methods=["POST"])
 def hello():
